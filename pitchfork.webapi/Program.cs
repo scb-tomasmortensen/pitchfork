63b2f6a70c5ca3f9adf7ec1b3d376bfa99205035
Tomas Mortensen <tomas@evosolution.se>
2019-10-27 10:17:28 +0100

    added EFCore, SearchController and Serilog

--- pitchfork.webapi/Program.cs
+++ pitchfork.webapi/Program.cs
@@ -1,18 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
 
 namespace pitchfork.webapi
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+           .ReadFrom.Configuration(Configuration)
+           .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
--- pitchfork.webapi/Startup.cs
+++ pitchfork.webapi/Startup.cs
@@ -1,30 +1,24 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace pitchfork.webapi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddSpaStaticFiles(configuration =>
                 configuration.RootPath = "pitchfork.web/build"
             );
+            services.AddSingleton(Log.Logger);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -33,15 +27,6 @@ namespace pitchfork.webapi
             }
 
             app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
@@ -55,6 +40,8 @@ namespace pitchfork.webapi
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
+
+           
         }
     }
 }
